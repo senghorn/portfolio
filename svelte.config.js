f286821e7cf5d4a51115a211abc9da1cec3e827c
senghorn <rithsenghorn@gmail.com>
2024-04-03 13:19:08 -0600

    deployment yml

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex'
-
+import adapter from '@sveltejs/adapter-static';
+const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.svx'],
@@ -13,7 +14,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
 	}
 };
 
